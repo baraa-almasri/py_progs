f9b0b45f325ca3e6e837c89248f60114a9538bf0
Baraa Al-Masri <baraa.masri@asu.edu.jo>
2020-05-03 09:54:29 +0300

    created @ Sun  3 May 09:54:07 EEST 2020

---
+++ polygon_generator.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+import math
+# reminder    x = radius*cos(theta), y = radius*sin(theta)
+# hence    theta = arccos(x/radius) or arcsin(y/radius)
+def genXcartesian(radius, angle):
+    return radius*math.cos(angle)
+
+def genYcartesian(radius, angle):
+    return radius*math.sin(angle)
+
+# radius = S/2sin(pi/n)
+# S = radius*2sin(pi/n)
+radius = float(input("Enter the radius of the Circumferting circle: "))
+print("Enter the cartesian coordinates of polygon's center (x,y): ")
+x, y = float(input()), float(input())
+noOfSides = int(input("Enter number of sides of the polygon: "))
+
+
+# the angle between two neighbouring diameters of the polygon
+angle = (math.pi*2)/noOfSides
+theta = angle
+for i in range(1, noOfSides+1):
+    print("for point ", i)
+    print("x:", x+genXcartesian(radius, theta))
+    print("y:", y+genYcartesian(radius, theta))
+
+    theta += angle
